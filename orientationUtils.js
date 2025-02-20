afcbba312a7ab6ee11364dd6b596de86d5bc625d
Cosmin-Valentin <cosminv.nicola@gmail.com>
2025-02-20 09:57:16 +0200

    Mobile modal adjustment

--- orientationUtils.js
+++ orientationUtils.js
@@ -5,8 +5,8 @@ export function showLandscapeModal() {
       position: fixed;
       top: 0;
       left: 0;
-      width: 100%;
-      height: 100%;
+      width: 100vw;
+      height: 100vh;
       background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent backdrop */
       display: flex;
       justify-content: center;
